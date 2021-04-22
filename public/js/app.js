bcd9d3ea6641ddf476eab9fa497d292e9406a86a
Dipesh Viradiya <dipesh40939@gmail.com>
2021-04-22 14:35:11 +0530

    js/app.js modified

--- public/js/app.js
+++ public/js/app.js
@@ -29,7 +29,7 @@ const messageTwo = document.querySelector('#p-2')
 
 weatherForm.addEventListener('submit',(e) => {   
     e.preventDefault()
-    messageOne.textContent = "Loading..."
+    messageOne.textContent = "Loading...."
     fetch('http://127.0.0.1:3000/weather?address='+search.value)
         .then((response) => {
             response.json()
